0891059cef2599fcce832ac41648f48a7255050b
Henrique <tomiattihtm@gmail.com>
2024-03-01 16:00:34 -0300

    First Steps in Java with Spring Boot

---
+++ src/main/java/project/springboot/Startup.java
@@ -0,0 +1,13 @@
+package project.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
